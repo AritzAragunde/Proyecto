0dbb7847d43b553bf1f97de88061e7d4bf68ca72
“Ibai <ibai.jimenez.50586@ikasle.egibide.org>
2025-11-12 10:44:53 +0100

    RamaIbai

--- Programacion/Proyecto/src/Main.java
+++ Programacion/Proyecto/src/Main.java
@@ -1,16 +1,77 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    static int NumeroJugadores = 0;
+    static String NombreEquipo;
+
+    public static void equipos() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce el nombre del equipo: ");
+        NombreEquipo = sc.nextLine();
+
+        System.out.println("Introduce el numero de jugadores del equipo: ");
+        NumeroJugadores = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("\nEquipo " + NombreEquipo + " registrado con " + NumeroJugadores + " jugadores.");
+
+
+        jugadores();
+    }
+
+    public static void jugadores() {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 1; i <= NumeroJugadores; i++) {
+            System.out.println("\nJugador " + i + " del equipo " + NombreEquipo + ":");
+            System.out.print("Nombre: ");
+            String nombre = sc.nextLine();
+
+            System.out.print("Apellido: ");
+            String apellido = sc.nextLine();
+
+            System.out.print("Edad: ");
+            int edad = sc.nextInt();
+
+            System.out.print("Dorsal: ");
+            int dorsal = sc.nextInt();
+            sc.nextLine();
+
+            System.out.println("Jugador registrado: " + nombre + " " + apellido + ", Edad: " + edad + ", Dorsal: " + dorsal);
+        }
+
+        System.out.println("\nTodos los jugadores del equipo " + NombreEquipo + " han sido registrados.");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean salir = false;
+
+        while (!salir) {
+            System.out.println("\n======== MENU ========\n" +
+                    "1- Inscribir Equipo y Jugadores\n" +
+                    "2- Salir");
+
+            try {
+                int respuesta = sc.nextInt();
+                sc.nextLine();
+
+                switch (respuesta) {
+                    case 1:
+                        equipos();
+                        break;
+                    case 2:
+                        salir = true;
+                        System.out.println("Saliendo del programa...");
+                        break;
+                    default:
+                        System.out.println("Opción no válida, intenta de nuevo.");
+                }
+            } catch (Exception e) {
+                System.out.println("No has escogido una opción correcta.");
+                sc.nextLine();
+            }
         }
     }
-    public static void  fUrtzi(){}
 }
